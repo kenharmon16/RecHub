2a8a5727f2fbe5e0b76ac25e2642735398c17db6
Ken Harmon <48740050+kenharmon16@users.noreply.github.com>
2025-03-21 22:21:28 -0500

    initial git commit

---
+++ src/main/java/com/api/rechub/RechubApplication.java
@@ -0,0 +1,13 @@
+package com.api.rechub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RechubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RechubApplication.class, args);
+	}
+
+}
